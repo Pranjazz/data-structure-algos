9076a2da139f1f69f3b139f9cd0e8c1a5a394a3f
Pranjazz <rajauriyapranjal1521@gmail.com>
2026-03-23 11:36:51 +0530

    Time: 188 ms (5.01%) | Memory: 11.9 MB (7.94%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool right(vector<int> &need, vector<int> &have){
+        for(int i=0;i<256;i++){
+            if( have[i] < need[i]){
+                return false;
+            }
+        }
+           return true;
+    }
+    string minWindow(string s, string t) {
+
+        vector<int> need (256,0);
+        vector<int> have (256,0);
+        int high =0;
+        int low =0;
+        int res=INT_MAX;
+        int start=0;
+        int len =high-low+1;
+
+        for(char c : t){
+           need[c]++;
+        }
+
+        for(high=0;high<s.size();high++){
+            have[s[high]]++;
+
+            while(right(need,have)){
+                len = high-low+1;
+                if(res > len){
+                    res = len;
+                    start = low;
+                }
+                have[s[low]]--;
+                low++;
+            }
+        }
+        return res == INT_MAX ? "":s.substr(start,res);
+    }
+};
