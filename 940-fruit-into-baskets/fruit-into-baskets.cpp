6983139cc956668e3fd78a00fced560294fb1671
Pranjazz <rajauriyapranjal1521@gmail.com>
2026-09-23 09:24:15 +0530

    Time: 49 ms (33.38%) | Memory: 84.8 MB (62.68%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> f;
         
-        int low=0;
-        int k=2;
+        unordered_map<int,int> f;
+        int low = 0;
         int res = -1;
 
-        for(int high =0;high<fruits.size();high++){
-
+        for(int high=0;high<fruits.size();high++){
             f[fruits[high]]++;
 
-            while(f.size() > k){
+            while(f.size() > 2){
 
                 f[fruits[low]]--;
-                
+
                 if(f[fruits[low]] == 0){
                     f.erase(fruits[low]);
                 }
 
                 low++;
             }
-            if(f.size() == k || f.size() < k){
-                res = max(res,high-low+1);
+
+            if(f.size() < 2 || f.size() == 2){
+                res = max(res, high - low + 1);
             }
         }
-        return res;    
 
+        return res;
+        
     }
 };
